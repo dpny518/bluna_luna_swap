c0016a18b4cef62899148f9d41400671a7d26e2b
Yondu Tsai <5721671+yondu22@users.noreply.github.com>
2021-06-08 16:58:01 +0800

    Update swapbot.py

--- swapbot.py
+++ swapbot.py
@@ -8,17 +8,33 @@ from terra_sdk.core.auth import StdFee
 from terra_sdk.core.bank import MsgSend
 from terra_sdk.core.wasm import MsgExecuteContract
 from terra_sdk.core.coins import Coins
-
+from contact_addresses import contact_addresses_lookup
 from time import sleep
 from pprint import pprint, pp
-
+from terraswap_swap_watch import run_terra_swap_price_watcher, get_luna_price_prices
 
 import os
 SEED = os.environ.get("MNEMONIC")
+NETWORK = 'TESTNET'
 MILLION = 1000000
 
+
+if NETWORK == 'MAINNET':
+    chain_id = 'columbus-4'
+    public_node_url = 'https://lcd.terra.dev'
+    contact_addresses = contact_addresses_lookup(network='MAINNET')
+    tx_look_up = f'https://finder.terra.money/{chain_id}/tx/'
+
+else:
+    chain_id = 'tequila-0004'
+    public_node_url = 'https://tequila-fcd.terra.dev'
+    contact_addresses = contact_addresses_lookup(network='TESTNET')
+    tx_look_up = f'https://finder.terra.money/{chain_id}/tx/'
+
+
+
 class BondedLunaToken:
-    bLunaContract = 'terra1kc87mu460fwkqte29rquh4hc20m54fxwtsx7gp'
+    bLunaContract = contact_addresses['bLunaToken']
     
     def __init__(self, terra, wallet=None) -> None:
         self.terra = terra
@@ -36,8 +52,8 @@ class BondedLunaToken:
         return result   
 
 class TerraSwap:
-    terraSwapContract = "terra1jxazgm67et0ce260kvrpfv50acuushpjsz2y0p"
-    bLunaContract = 'terra1kc87mu460fwkqte29rquh4hc20m54fxwtsx7gp'
+    terraSwapContract = contact_addresses['terraswapblunaLunaPair']
+    bLunaContract = contact_addresses['bLunaToken']
 
     def __init__(self, terra, wallet=None) -> None:
         self.terra = terra
@@ -61,7 +77,7 @@ class TerraSwap:
         )
         return result
 
-    def swap(self, amount, return_amount):
+    def swap_luna(self, amount, return_amount):
         
 
         increase_allowance = MsgExecuteContract(
@@ -75,7 +91,8 @@ class TerraSwap:
             }
         )
 
-        swap_bluna = MsgExecuteContract(
+      
+        swap_luna_to_bluna = MsgExecuteContract(
             self.wallet.key.acc_address,
             self.terraSwapContract,
             {
@@ -92,8 +109,34 @@ class TerraSwap:
             },
             {"uluna": amount * 1000000},
         )
+        tx = self.wallet.create_and_sign_tx(msgs=[increase_allowance, swap_luna_to_bluna], gas_prices="0.15uusd", gas_adjustment=1.5)
+        # fee = self.terra.tx.estimate_fee(tx)
+        result = self.terra.tx.broadcast(tx)
+        return [tx, result]
+        # return result
+    
 
-        tx = self.wallet.create_and_sign_tx(msgs=[increase_allowance, swap_bluna], gas_prices="0.15uusd", gas_adjustment=1.5)
+    def swap_bluna(self, amount, return_amount):
+        
+
+        swap_bluna_to_luna = MsgExecuteContract(
+            self.wallet.key.acc_address,
+            self.bLunaContract,
+  
+{
+  "send": {
+    "contract": "terra13e4jmcjnwrauvl2fnjdwex0exuzd8zrh5xk29v",
+    "amount": "1000000",
+    "msg": {
+      "swap": {
+        "belief_price": "0.759270887352293339",
+        "max_spread": "0.1"
+      }
+    }
+  }
+}
+            )
+        tx = self.wallet.create_and_sign_tx(msgs=[swap_bluna_to_luna], gas_prices="0.15uusd", gas_adjustment=1.5)
         # fee = self.terra.tx.estimate_fee(tx)
         result = self.terra.tx.broadcast(tx)
         return [tx, result]
@@ -101,12 +144,12 @@ class TerraSwap:
     
 
 
-    
 
-bot_token = 'xxxxxxxxx'
 
-#terra = LCDClient(chain_id="columbus-4", url="https://lcd.terra.dev")
-terra = LCDClient(chain_id="tequila-0004", url="https://lcd.terra.dev")
+
+
+# Connect to Testnet
+terra = LCDClient(chain_id=chain_id, url=public_node_url)    
 mk = MnemonicKey(mnemonic=SEED)
 wallet = terra.wallet(mk)
 swap = TerraSwap(terra, wallet)
@@ -115,9 +158,9 @@ bLunaToken = BondedLunaToken(terra, wallet)
 num_bLunaTokens = (int) (bLunaToken.get_balance()['balance']) / MILLION
 pp(num_bLunaTokens)
 
-swap_amount = 5
-buy_min_rate = 0.03
-sell_max_rate = 0.01
+swap_amount = 100
+luna_to_bluna_min_rate = 1.03
+bluna_to_luna_min_rate = .98
 
 coins = terra.bank.balance(wallet.key.acc_address)
 pp(coins)
@@ -127,28 +170,34 @@ pp(num_Luna)
 # terra.tx.search()
 while True:
     rate = swap.get_exchange_rate(swap_amount)
-    rate['zProfit'] = int(rate['return_amount']) / MILLION - swap_amount
-    rate['zzPR'] = '{:.2%}'.format(rate['zProfit'] / swap_amount)
-    rate['zzzAPR'] = '{:.2%}'.format(rate['zProfit'] / swap_amount / 21 * 365)
-    rate['zzzzAPY'] = '{:.2%}'.format((1 + rate['zProfit'] / swap_amount) ** 17 - 1)
-    pprint(rate)
+    return_amount = int(swap.get_exchange_rate(swap_amount)['return_amount'])
+    exchange_rate = round(return_amount * 100 / (swap_amount * 1000000) - 100, 3)
 
-    if(rate['zProfit'] / swap_amount > buy_min_rate):
+    terraswap_prices = run_terra_swap_price_watcher()
+    print('luna to Bluna')
+    luna_bluna_ratio=terraswap_prices[0]
+    bluna_luna_ratio=terraswap_prices[1]
+    print(luna_bluna_ratio)
+    print('bluna to luna')
+    print(bluna_luna_ratio)
+
+    if(luna_bluna_ratio>luna_to_bluna_min_rate):
         print('swap Luna -> bLuna')
         if(num_Luna < swap_amount):
-            print('not enough Luna to swap')
-        else:
-            print('go swap')
-    #     swap_result = swap.swap(swap_amount, rate['return_amount'])
-    #     pprint(swap_result)
-    #     break
-    elif(rate['zProfit'] / swap_amount < sell_max_rate):
-        print('swap bLuna -> Luna')
-        if(num_bLunaTokens < swap_amount):
             print('not enough bLuna to swap')
         else:
-            print('go swap')
-
-    # break
+            swap_result = swap.swap_luna(swap_amount, rate['return_amount'])
+            print(swap_result)
+    elif(bluna_luna_ratio>bluna_to_luna_min_rate):
+        print('swap bluna -> Luna')
+        if(num_Luna < swap_amount):
+            print('not enough bLuna to swap')
+        else:
+            print("bluna to luna swap doesn't work")
+            #swap_result = swap.swap_bluna(swap_amount, rate['return_amount'])
+            #print(swap_result)
+    else:
+        print("no swapping right now")
     sleep(5)
+
 
